25e8b1d18e86ff5bb2b3bc04c8af826b706df2b7
Franco Pertusati <pertusatifranco836@gmail.com>
2025-08-16 14:05:21 -0300

    Maquetado de login y estilos compartidos para auth

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { GoogleAuthBtnComponent } from "../shared/google-auth-btn/google-auth-btn.component";
+import { FormArray, FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from "@angular/forms"
+
+export interface ItemForm {
+  id: FormControl<number>
+  name: FormControl<string>
+  value: FormControl<string>
+}
+
+export type CustomFormGroup = FormGroup<ItemForm>
 
 @Component({
   selector: 'app-login',
-  imports: [GoogleAuthBtnComponent],
+  imports: [GoogleAuthBtnComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: '../auth.component.css'
 })
 export class LoginComponent {
+  fb = inject(NonNullableFormBuilder)
 
+  form: FormGroup<{ items: FormArray<CustomFormGroup> }> = this.fb.group({
+    items: this.fb.array<CustomFormGroup>([])
+  })
+
+  items = signal(this.form.controls.items.controls)
+
+  addItem() {
+    const id = this.items().length + 1
+    const itmeForm = this.fb.group<ItemForm>({
+      id: this.fb.control(id),
+      name: this.fb.control("", { validators: [Validators.required] }),
+      value: this.fb.control("", { validators: [Validators.required] })
+    })
+
+    this.form.controls.items.push(itmeForm)
+
+    this.items.set([... this.form.controls.items.controls])
+  }
 }
