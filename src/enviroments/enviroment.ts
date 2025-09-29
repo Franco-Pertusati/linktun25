7f8c1ab0306a070fd98c70a1434328a33b414496
Franco Pertusati <pertusatifranco836@gmail.com>
2025-09-29 14:02:07 -0300

    Correccion de apiUrl

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
   production: false,
-  // API_URL: 'https://linktun-api-2025-production.up.railway.app',
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://linktun-api-2025-production.up.railway.app',
+  // API_URL: 'http://localhost:3000',
 }
